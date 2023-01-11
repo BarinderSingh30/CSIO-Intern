45755a91f1d450fa2795ce06ddee72d395c3d57e
BarinderSingh30 <78806961+BarinderSingh30@users.noreply.github.com>
2023-01-11 23:22:29 +0530

    Feature detection and matching

---
+++ CSIO/SURF.py
@@ -0,0 +1,29 @@
+import cv2 
+
+#sift
+surf = cv2.xfeatures2d.SURF_create(400)
+
+#feature matching
+bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
+
+
+imgL = cv2.imread('teddy-png-2/teddy/imgL.png')  
+imgR = cv2.imread('teddy-png-2/teddy/imgR.png') 
+
+imgL = cv2.cvtColor(imgL, cv2.COLOR_BGR2GRAY)
+imgR = cv2.cvtColor(imgR, cv2.COLOR_BGR2GRAY)
+
+keypoints_L, descriptors_L = surf.detectAndCompute(imgL,None)
+keypoints_R, descriptors_R = surf.detectAndCompute(imgR,None)
+
+
+
+matches = bf.match(descriptors_L,descriptors_R)
+matches = sorted(matches, key = lambda x:x.distance)
+print(len(matches))
+
+img3 = cv2.drawMatches(imgL, keypoints_L, imgR, keypoints_R, matches[205:215], imgR, flags=2)
+
+cv2.imshow('SIFT', img3)
+
+cv2.waitKey(0)
